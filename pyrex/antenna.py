516092e7434864375e4d00d3d7a73702f9ee4faa
Ben Hokanson-Fasig <bhokansonfasig@gmail.com>
2017-08-10 15:13:13 -0500

    Added antenna class and related tests

---
+++ pyrex/antenna.py
@@ -0,0 +1,60 @@
+"""Module containing antenna class capable of receiving signals"""
+
+import warnings
+import numpy as np
+from pyrex.digsig import GaussianNoise
+
+class Antenna:
+    """Antenna with a given name, position (m), center frequency (MHz),
+    bandwidth (MHz), effective height (m) and trigger threshold (V)."""
+    def __init__(self, name, position, center_frequency, bandwidth,
+                 effective_height, threshold, noisy=True):
+        # gets the time of the hit [ns]
+        # induced signal strength [V]
+        self.name = name
+        self.pos = position
+        self.center_frequency = center_frequency
+        self.bandwidth = bandwidth
+        self.effective_height = effective_height
+        self.threshold = threshold
+        self.noisy = noisy
+
+        self.signals = []
+        self._waveforms_generated = 0
+        self._noises = []
+
+        # Get critical frequencies in rad/s
+        self.f_low = 2*np.pi * (self.center_frequency - self.bandwidth/2)*1e6
+        self.f_high = 2*np.pi * (self.center_frequency + self.bandwidth/2)*1e6
+        
+        self.clear()
+
+    @property
+    def waveforms(self):
+        """Signal + (optional) noise at each antenna hit."""
+        if not(self.noisy):
+            return self.signals
+
+        if self._waveforms_generated!=len(self.signals):
+            new_signal = self.signals[-1]
+            new_noise = GaussianNoise(new_signal.times, max(new_signal.values)/5)
+            # new_noise = ThermalNoise(signal.times, [self.f_low, self.f_high])
+            self._noises.append(new_noise)
+
+        return [s+n for s,n in zip(self.signals, self._noises)]
+
+    def is_hit(self):
+        """Test for whether the antenna has received a signal."""
+        return len(self.signals)>0
+
+    def isHit(self):
+        """Deprecated. Replaced by is_hit."""
+        warnings.warn("Antenna.isHit has been replaced by Antenna.is_hit",
+                      DeprecationWarning, stacklevel=2)
+        return self.is_hit()
+
+    def clear(self):
+        """Reset the antenna to having received no signals."""
+        self.signals.clear()
+        self._waveforms_generated = 0
+        self._noises.clear()
---
+++ tests/test_antenna.py
@@ -0,0 +1,63 @@
+"""File containing tests of pyrex digsig module"""
+
+import pytest
+
+from pyrex.antenna import Antenna
+from pyrex.digsig import Signal
+
+import numpy as np
+
+
+
+@pytest.fixture
+def antenna():
+    """Fixture for forming basic Antenna object"""
+    return Antenna("ant", [0,0,-250], 250, 100, 1.0, 5E-6)
+
+
+class TestAntenna:
+    """Tests for Antenna class"""
+    def test_creation(self, antenna):
+        """Test that the antenna's creation goes as expected"""
+        assert antenna.name == "ant"
+        assert np.array_equal(antenna.pos, [0,0,-250])
+        assert antenna.center_frequency == 250
+        assert antenna.bandwidth == 100
+        assert antenna.effective_height == 1
+        assert antenna.threshold == pytest.approx(5e-6)
+        assert antenna.noisy
+        assert antenna.signals == []
+        assert antenna.f_low == pytest.approx(2*np.pi*200*1e6)
+        assert antenna.f_high == pytest.approx(2*np.pi*300*1e6)
+
+    def test_is_hit(self, antenna):
+        """Test that is_hit returns true when there is a signal and false otherwise"""
+        assert not(antenna.is_hit())
+        antenna.signals.append(Signal([0],[0]))
+        assert antenna.is_hit()
+
+    def test_clear(self, antenna):
+        """Test that clear emptys signals list"""
+        antenna.signals.append(Signal([0],[0]))
+        antenna.clear()
+        assert antenna.signals == []
+
+    def test_no_waveforms(self, antenna):
+        """Test that waveforms returns an empty list if there are no signals"""
+        assert antenna.waveforms == []
+
+    def test_waveforms_exist(self, antenna):
+        """Test that waveforms returns a waveform when there is a signal"""
+        antenna.signals.append(Signal([0],[0]))
+        assert antenna.waveforms != []
+        assert isinstance(antenna.waveforms[0], Signal)
+
+    def test_noises_not_recalculated(self, antenna):
+        """Test that noise signals aren't recalculated every time"""
+        antenna.signals.append(Signal([0],[1]))
+        waveforms1 = antenna.waveforms
+        noises1 = antenna._noises
+        waveforms2 = antenna.waveforms
+        noises2 = antenna._noises
+        assert noises1 == noises2
+
