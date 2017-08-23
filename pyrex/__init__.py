703c1c8c4f0952e594104799ad78bf09c16e7f5b
Ben Hokanson-Fasig <bhokansonfasig@gmail.com>
2017-08-23 15:30:01 -0500

    Create nice imports for the package

---
+++ pyrex/__init__.py
@@ -0,0 +1,7 @@
+from pyrex.signals import Signal, EmptySignal, FunctionSignal, AskaryanSignal, ThermalNoise
+from pyrex.antenna import Antenna, DipoleAntenna
+from pyrex.ice_model import IceModel
+from pyrex.earth_model import prem_density, slant_depth
+from pyrex.particle import Particle, ShadowGenerator
+from pyrex.ray_tracing import PathFinder
+from pyrex.kernel import EventKernel
