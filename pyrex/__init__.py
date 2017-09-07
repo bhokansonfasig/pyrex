461ef9ab34f141e7915f9c93df44f3037fc761ae
Ben Hokanson-Fasig <bhokansonfasig@gmail.com>
2017-09-07 15:14:59 -0500

    Get smart about having version number in only one place Also, remove unnecessary pyrex paths in __init__

--- pyrex/__init__.py
+++ pyrex/__init__.py
@@ -1,7 +1,9 @@
-from pyrex.signals import Signal, EmptySignal, FunctionSignal, AskaryanSignal, ThermalNoise
-from pyrex.antenna import Antenna, DipoleAntenna
-from pyrex.ice_model import IceModel
-from pyrex.earth_model import prem_density, slant_depth
-from pyrex.particle import Particle, ShadowGenerator
-from pyrex.ray_tracing import PathFinder
-from pyrex.kernel import EventKernel
+from .signals import Signal, EmptySignal, FunctionSignal, AskaryanSignal, ThermalNoise
+from .antenna import Antenna, DipoleAntenna
+from .ice_model import IceModel
+from .earth_model import prem_density, slant_depth
+from .particle import Particle, ShadowGenerator
+from .ray_tracing import PathFinder
+from .kernel import EventKernel
+
+__version__ = "1.0.2"
