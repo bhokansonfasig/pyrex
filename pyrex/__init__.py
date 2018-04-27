7f9d30bd55ba2e26be612c9f9e1ff07bbd4bcd17
Ben Hokanson-Fasig <bhokansonfasig@gmail.com>
2018-04-27 14:10:02 -0500

    Include ListGenerator and FileGenerator in base pyrex package

--- pyrex/__init__.py
+++ pyrex/__init__.py
@@ -12,7 +12,7 @@ from .antenna import Antenna, DipoleAntenna
 from .detector import AntennaSystem, Detector
 from .ice_model import IceModel
 from .earth_model import prem_density, slant_depth
-from .particle import Particle, ShadowGenerator
+from .particle import Particle, ShadowGenerator, ListGenerator, FileGenerator
 from .ray_tracing import RayTracer, RayTracePath
 from .kernel import EventKernel
 
