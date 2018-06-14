2cdb5804ec4e7c04c90f4fe60f67ccf8ba871f4b
Benjamin Hokanson-Fasig <bhokansonfasig@gmail.com>
2018-06-14 16:32:56 -0500

    Add more options to EventKernel for increased modularity

--- pyrex/kernel.py
+++ pyrex/kernel.py
@@ -14,10 +14,14 @@ logger = logging.getLogger(__name__)
 class EventKernel:
     """Kernel for generation of events with a given particle generator,
     list of antennas, and optionally a non-default ice_model."""
-    def __init__(self, generator, antennas, ice_model=IceModel):
+    def __init__(self, generator, antennas,
+                 ice_model=IceModel, ray_tracer=RayTracer,
+                 signal_times=np.linspace(-20e-9, 80e-9, 2000, endpoint=False)):
         self.gen = generator
-        self.ice = ice_model
         self.antennas = antennas
+        self.ice = ice_model
+        self.ray_tracer = ray_tracer
+        self.signal_times = signal_times
 
     def event(self):
         """Generate particle, propagate signal through ice to antennas,
@@ -26,7 +30,7 @@ class EventKernel:
         logger.info("Processing event for %s", p)
         n = self.ice.index(p.vertex[2])
         for ant in self.antennas:
-            rt = RayTracer(p.vertex, ant.position, ice_model=self.ice)
+            rt = self.ray_tracer(p.vertex, ant.position, ice_model=self.ice)
 
             # If no path(s) between the points, skip ahead
             if not rt.exists:
@@ -53,9 +57,8 @@ class EventKernel:
                 # FIXME: Use shower energy for AskaryanSignal
                 # Dependent on shower type / neutrino type
 
-                times = np.linspace(-20e-9, 80e-9, 2000, endpoint=False)
-                pulse = AskaryanSignal(times=times, energy=p.energy,
-                                       theta=psi, n=n)
+                pulse = AskaryanSignal(times=self.signal_times,
+                                       energy=p.energy, theta=psi, n=n)
 
                 path.propagate(pulse)
                 # Dividing by path length scales Askaryan pulse properly
