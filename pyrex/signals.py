Marginal speed-up in noise calculation, and fix filtered signals to real

pyrex/signals.py
@@ -131,7 +131,7 @@ class Signal:
             responses.imag[true_freqs<0] *= -1
 
         filtered_vals = scipy.fftpack.ifft(responses*spectrum)
-        self.values = filtered_vals[:len(self.times)]
+        self.values = np.real(filtered_vals[:len(self.times)])
 
         # Issue a warning if there was significant signal in the (discarded)
         # imaginary part of the filtered values
@@ -499,6 +499,9 @@ class ThermalNoise(FunctionSignal):
         # https://www.phys.hawaii.edu/elog/anita_notes/060228_110754/noise_simulation.ps
 
         self.f_min, self.f_max = f_band
+        if self.f_min>=self.f_max:
+            raise ValueError("Frequency band must have smaller frequency as "+
+                             "first value and larger frequency as second value")
         # If number of frequencies is unspecified (or invalid),
         # determine based on the FFT bin size of the times array
         if n_freqs<1:
@@ -521,6 +524,10 @@ class ThermalNoise(FunctionSignal):
             self.amps = [f_amplitude(f) for f in self.freqs]
         else:
             self.amps = np.full(len(self.freqs), f_amplitude, dtype="float64")
+            # If the frequency range extends to zero, force the zero-frequency
+            # (DC) amplitude to zero
+            if 0 in self.freqs:
+                self.amps[np.where(self.freqs==0)[0]] = 0
 
         self.phases = np.random.rand(len(self.freqs)) * 2*np.pi
 
@@ -535,14 +542,16 @@ class ThermalNoise(FunctionSignal):
                              " must be provided to calculate noise amplitude")
 
         def f(ts):
-            # Set the time-domain signal by adding sinusoidal signals of each
-            # frequency with the corresponding phase
-            values = np.zeros(len(ts))
-            for freq, amp, phase in zip(self.freqs, self.amps, self.phases):
-                # Skip zero-frequency component if it exists
-                if freq==0:
-                    continue
-                values += amp * np.cos(2*np.pi*freq * ts + phase)
+            """Set the time-domain signal by adding sinusoidal signals of each
+            frequency with the corresponding phase."""
+            # This method is nicer than an inverse fourier transform because
+            # results are consistant for differing ranges of ts around the same
+            # time. The price payed is that the fft would be an order of
+            # magnitude faster, but not reproducible for a slightly different
+            # time array.
+            values = sum(amp * np.cos(2*np.pi*freq * ts + phase)
+                         for freq, amp, phase
+                         in zip(self.freqs, self.amps, self.phases))
 
             # Normalization calculated by guess-and-check,
             # but seems to work fine