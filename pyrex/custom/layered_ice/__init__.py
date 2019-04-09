bbf8a9224f7a83f200b16e322c18cfc5fdbaa299
Benjamin Hokanson-Fasig <bhokansonfasig@gmail.com>
2019-04-09 14:22:45 -0500

    Remove UniformIce references from layered_ice init

--- pyrex/custom/layered_ice/__init__.py
+++ pyrex/custom/layered_ice/__init__.py
@@ -3,6 +3,5 @@ Customizations of pyrex package for stratified ice layers.
 
 """
 
-from .ice_model import UniformIce, LayeredIce
-from .ray_tracing import (UniformRayTracer, UniformRayTracePath,
-                          LayeredRayTracer, LayeredRayTracePath)
+from .ice_model import LayeredIce
+from .ray_tracing import LayeredRayTracer, LayeredRayTracePath
