2dff43ff216421f00ae7e741ee595800360ce212
Benjamin Hokanson-Fasig <bhokansonfasig@gmail.com>
2020-02-13 11:33:27 -0600

    Replace AlbrechtStation with PhasedArrayStation similar to ARA05

--- pyrex/custom/ara/__init__.py
+++ pyrex/custom/ara/__init__.py
@@ -4,5 +4,5 @@ Customizations of pyrex package specific to ARA (Askaryan Radio Array).
 """
 
 from .antenna import HpolAntenna, VpolAntenna
-from .detector import (ARAString, PhasedArrayString, RegularStation,
-                       AlbrechtStation, HexagonalGrid)
+from .detector import (ARAString, PhasedArrayString,RegularStation,
+                       HexagonalGrid)
