de15326850bc5e52956eaf94a92b8f6c6d059397
Benjamin Hokanson-Fasig <bhokansonfasig@gmail.com>
2020-03-17 14:58:24 -0500

    Add many more tests of FunctionSignal

--- tests/test_signals.py
+++ tests/test_signals.py
@@ -45,6 +45,16 @@ class TestSignal:
         assert new.times[0] != signals.times[0]
         assert new.values[0] != signals.values[0]
 
+    def test_copy(self, signals):
+        """Test that a copy of the signal is independent"""
+        new = signals.copy()
+        assert new is not signals
+        assert np.array_equal(new.times, signals.times)
+        assert new.times is not signals.times
+        assert np.array_equal(new.values, signals.values)
+        assert new.values is not signals.values
+        assert new.value_type == signals.value_type
+
     def test_addition(self, signals):
         """Test that signal objects can be added"""
         expected = Signal(signals.times, 2*signals.values, signals.value_type)
@@ -198,6 +208,12 @@ class TestSignal:
         assert np.array_equal(new.values, expected.values)
         assert new.value_type == signal.value_type
 
+    def test_shift(self, signal):
+        """Test that shifting the signal changes the times accordingly"""
+        old_times = np.array(signal.times)
+        signal.shift(5)
+        assert np.array_equal(signal.times, old_times+5)
+
     def test_spectrum(self, signal):
         """Test that spectrum attribute returns expected values"""
         expected = [7, -0.5, -0.5, -0.5, -0.5]
@@ -254,6 +270,68 @@ class TestEmptySignal:
         assert np.array_equal(empty_signal.values, np.zeros(5))
         assert empty_signal.value_type == Signal.Type.undefined
 
+    def test_copy(self, empty_signal):
+        """Test that a copy of the EmptySignal is independent"""
+        new = empty_signal.copy()
+        assert new is not empty_signal
+        assert np.array_equal(new.times, empty_signal.times)
+        assert new.times is not empty_signal.times
+        assert np.array_equal(new.values, empty_signal.values)
+        assert new.values is not empty_signal.values
+        assert new.value_type == empty_signal.value_type
+
+    def test_addition(self, signal):
+        """Test that EmptySignal objects can be added to other signals"""
+        empty = EmptySignal(signal.times, signal.value_type)
+        signal_sum = empty + signal
+        assert np.array_equal(signal_sum.times, signal.times)
+        assert np.array_equal(signal_sum.values, signal.values)
+        assert signal_sum.value_type == signal.value_type
+
+    def test_summation(self, signal):
+        """Test that sum() can be used with EmptySignal objects"""
+        empty = EmptySignal(signal.times, signal.value_type)
+        signal_sum = sum([empty, signal])
+        assert np.array_equal(signal.times, signal_sum.times)
+        assert np.array_equal(signal.values, signal_sum.values)
+        assert signal_sum.value_type == signal.value_type
+
+    def test_addition_type_failure(self, empty_signal):
+        """Test that EmptySignal objects cannot be added to other types
+        (except left-add of iteger 0 for use of sum())"""
+        assert 0+empty_signal == empty_signal
+        with pytest.raises(TypeError):
+            signal_sum = empty_signal + 1
+        with pytest.raises(TypeError):
+            signal_sim = empty_signal + [1,1,1,1,1]
+        with pytest.raises(TypeError):
+            signal_sum = empty_signal + np.array([1,1,1,1,1])
+        with pytest.raises(TypeError):
+            signal_sim = empty_signal + "Why would you even try this?"
+
+    def test_addition_times_failure(self):
+        """Test that EmptySignal objects with different times cannot be added"""
+        signal_1 = EmptySignal([0,1,2,3,4])
+        signal_2 = EmptySignal([1,2,3,4,5])
+        with pytest.raises(ValueError):
+            signal_sum = signal_1 + signal_2
+
+    def test_addition_value_type_failure(self):
+        """Test that adding EmtpySignal objects with different value types fails"""
+        signal_1 = EmptySignal([0,1,2,3,4], value_type=Signal.Type.voltage)
+        signal_2 = EmptySignal([0,1,2,3,4], value_type=Signal.Type.field)
+        with pytest.raises(ValueError):
+            signal_sum = signal_1 + signal_2
+
+    def test_addition_value_type_coercion(self, empty_signal, signal):
+        """Test that adding EmptySignal objects with undefined value type
+        results in a sum with the value type of the other signal"""
+        empty_signal.value_type = Signal.Type.voltage
+        signal_sum_1 = signal + empty_signal
+        signal_sum_2 = empty_signal + signal
+        assert signal_sum_1.value_type == empty_signal.value_type
+        assert signal_sum_2.value_type == empty_signal.value_type
+
     def test_with_times(self, empty_signal):
         """Test that with_times method keeps all zeros"""
         empty_signal.value_type = Signal.Type.voltage
@@ -263,7 +341,7 @@ class TestEmptySignal:
 
 
 
-@pytest.fixture(params=[lambda x: x==1, np.cos])
+@pytest.fixture(params=[lambda x: np.where(x==1, 1, 0), np.cos])
 def function_signals(request):
     """Fixture for forming parameterized Signal objects for many value types"""
     ts = [0,1,2,3,4]
@@ -276,8 +354,118 @@ class TestFunctionSignal:
         """Test initialization of function signals"""
         assert np.array_equal(function_signals.times, [0,1,2,3,4])
         for i in range(5):
-            assert function_signals.values[i] == function_signals.function(i)
+            assert function_signals.values[i] == function_signals._functions[0](i)
         assert function_signals.value_type == Signal.Type.undefined
+
+    def test_copy(self, function_signals):
+        """Test that a copy of the FunctionSignal is independent"""
+        new = function_signals.copy()
+        assert new is not function_signals
+        assert np.array_equal(new.times, function_signals.times)
+        assert new.times is not function_signals.times
+        assert np.array_equal(new.values, function_signals.values)
+        assert new.values is not function_signals.values
+        assert new.value_type == function_signals.value_type
+
+    def test_addition(self, function_signals):
+        """Test that FunctionSignal objects can be added"""
+        expected = Signal(function_signals.times, 2*function_signals.values,
+                          function_signals.value_type)
+        signal_sum = function_signals + function_signals
+        assert np.array_equal(signal_sum.times, expected.times)
+        assert np.array_equal(signal_sum.values, expected.values)
+        assert signal_sum.value_type == expected.value_type
+
+    def test_summation(self, function_signals):
+        """Test that sum() can be used with FunctionSignal objects"""
+        signal_sum = sum([function_signals, function_signals])
+        assert np.array_equal(function_signals.times, signal_sum.times)
+        assert np.array_equal(2*function_signals.values, signal_sum.values)
+        assert signal_sum.value_type == function_signals.value_type
+
+    def test_addition_type_failure(self, function_signals):
+        """Test that FunctionSignal objects cannot be added to other types
+        (except left-add of iteger 0 for use of sum())"""
+        assert 0+function_signals == function_signals
+        with pytest.raises(TypeError):
+            signal_sum = function_signals + 1
+        with pytest.raises(TypeError):
+            signal_sim = function_signals + [1,1,1,1,1]
+        with pytest.raises(TypeError):
+            signal_sum = function_signals + np.array([1,1,1,1,1])
+        with pytest.raises(TypeError):
+            signal_sim = function_signals + "Why would you even try this?"
+
+    def test_addition_value_type_failure(self, function_signals):
+        """Test that adding FunctionSignal objects with different value types fails"""
+        new = function_signals.copy()
+        new.value_type = Signal.Type.voltage
+        function_signals.value_type = Signal.Type.field
+        with pytest.raises(ValueError):
+            signal_sum = new + function_signals
+
+    def test_addition_value_type_coercion(self, function_signals):
+        """Test that adding FunctionSignal objects with undefined value type
+        results in a sum with the value type of the other signal"""
+        function_signals.value_type = Signal.Type.voltage
+        undef_signal = function_signals.copy()
+        undef_signal.value_type = Signal.Type.undefined
+        signal_sum_1 = function_signals + undef_signal
+        signal_sum_2 = undef_signal + function_signals
+        assert signal_sum_1.value_type == function_signals.value_type
+        assert signal_sum_2.value_type == function_signals.value_type
+
+    @pytest.mark.parametrize("factor", [0, 2, np.pi])
+    def test_multiplication(self, function_signals, factor):
+        """Test that FunctionSignal types can be multiplied by scalar values"""
+        signal_product_1 = function_signals * factor
+        assert np.array_equal(signal_product_1.times, function_signals.times)
+        assert np.array_equal(signal_product_1.values,
+                              factor*function_signals.values)
+        assert signal_product_1.value_type == function_signals.value_type
+        signal_product_2 = factor * function_signals
+        assert np.array_equal(signal_product_2.times, function_signals.times)
+        assert np.array_equal(signal_product_2.values,
+                              factor*function_signals.values)
+        assert signal_product_2.value_type == function_signals.value_type
+        new = function_signals.copy()
+        new *= factor
+        assert np.array_equal(new.times, function_signals.times)
+        assert np.array_equal(new.values, factor*function_signals.values)
+        assert new.value_type == function_signals.value_type
+
+    def test_multiplication_uniqueness(self, function_signals):
+        """Test that multiplying a FunctionSignal doesn't change other signals which
+        used those same values"""
+        signal_product = function_signals * 2
+        function_signals *= 2
+        assert np.array_equal(signal_product.values, function_signals.values)
+
+    @pytest.mark.parametrize("factor", [2, np.pi])
+    def test_division(self, function_signals, factor):
+        """Test that FunctionSignal types can be divided by scalar values"""
+        signal_quotient = function_signals / factor
+        assert np.array_equal(signal_quotient.times, function_signals.times)
+        assert np.allclose(signal_quotient.values,
+                           function_signals.values/factor)
+        assert signal_quotient.value_type == function_signals.value_type
+        new = function_signals.copy()
+        new /= factor
+        assert np.array_equal(new.times, function_signals.times)
+        assert np.allclose(new.values, function_signals.values/factor)
+        assert new.value_type == function_signals.value_type
+
+    def test_resample(self, function_signals):
+        """Test FunctionSignal resampling"""
+        resampled = function_signals.copy()
+        resampled.resample(3)
+        expected_values = function_signals._functions[0](
+            np.asarray(function_signals.times[::2])
+        )
+        assert len(resampled.times) == 3
+        assert len(resampled.values) == 3
+        assert np.array_equal(resampled.times, function_signals.times[::2])
+        assert np.array_equal(resampled.values, expected_values)
 
     def test_with_times(self, function_signals):
         """Test that with_times method uses function to re-evaluate"""
@@ -286,8 +474,27 @@ class TestFunctionSignal:
         new = function_signals.with_times(times)
         assert np.array_equal(new.times, times)
         for i in range(19):
-            assert new.values[i] == function_signals.function(times[i])
+            assert new.values[i] == function_signals._functions[0](times[i])
         assert new.value_type == function_signals.value_type
+
+    def test_shift(self, function_signals):
+        """Test that shifting the FunctionSignal changes the times accordingly"""
+        old_times = np.array(function_signals.times)
+        function_signals.shift(5)
+        assert np.array_equal(function_signals.times, old_times+5)
+
+    def test_set_buffers(self):
+        """Test whether setting the FunctionSignal time buffers works as expected"""
+        signal = FunctionSignal([0, 1, 2, 3, 4], np.cos)
+        assert np.array_equal(signal._buffers[0], [0, 0])
+        signal.set_buffers(leading=1)
+        assert np.array_equal(signal._buffers[0], [1, 0])
+        signal.set_buffers(trailing=2)
+        assert np.array_equal(signal._buffers[0], [1, 2])
+        signal.set_buffers(trailing=1)
+        assert np.array_equal(signal._buffers[0], [1, 2])
+        signal.set_buffers(trailing=1, force=True)
+        assert np.array_equal(signal._buffers[0], [1, 1])
 
 
 
