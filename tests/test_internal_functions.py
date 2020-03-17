86543825b49d2eb22db852e509063ed1e72398db
Benjamin Hokanson-Fasig <bhokansonfasig@gmail.com>
2020-03-17 13:57:29 -0500

    Add tests of interpolation functions

--- tests/test_internal_functions.py
+++ tests/test_internal_functions.py
@@ -5,13 +5,15 @@ import pytest
 from config import SEED
 
 from pyrex.internal_functions import (normalize, get_from_enum,
-                                      flatten, mirror_func,
+                                      flatten, complex_interp,
+                                      complex_bilinear_interp, mirror_func,
                                       lazy_property, LazyMutableClass)
 
 from enum import Enum
 import inspect
 import types
 import numpy as np
+import scipy.signal
 
 
 
@@ -138,6 +140,186 @@ class Test_flatten:
         l3 = [1, 2, (3, [4, 5]), 6, [7, 8]]
         f3 = flatten(l3, dont_flatten=[tuple])
         assert list(f3) == [1, 2, (3, [4, 5]), 6, 7, 8]
+
+
+
+class Test_complex_interp:
+    """Tests for complex_interp function"""
+    def test_cartesian_interpolation(self):
+        """Test cartesian complex interpolation"""
+        steps = np.arange(5)
+        interp_steps = steps[:-1] + 0.5
+        real = np.array([1, 2, 3, 2, 1])
+        imag = np.array([3, 2, 1, 2, 3])
+        interp = complex_interp(interp_steps, steps, real+1j*imag,
+                                method='cartesian')
+        real_interp = np.interp(interp_steps, steps, real)
+        imag_interp = np.interp(interp_steps, steps, imag)
+        assert np.array_equal(np.real(interp), real_interp)
+        assert np.array_equal(np.imag(interp), imag_interp)
+
+    def test_cartesian_extrapolation(self):
+        """Test cartesian complex extrapolation"""
+        steps = np.arange(5)
+        extrap_steps = [steps[0]-1, steps[-1]+1]
+        real = np.array([1, 2, 3, 2, 1])
+        imag = np.array([3, 2, 1, 2, 3])
+        extrap = complex_interp(extrap_steps, steps, real+1j*imag,
+                                method='cartesian', outer=None)
+        assert np.array_equal(np.real(extrap), [real[0], real[-1]])
+        assert np.array_equal(np.imag(extrap), [imag[0], imag[-1]])
+        extrap2 = complex_interp(extrap_steps, steps, real+imag,
+                                 method='cartesian', outer=0)
+        assert np.array_equal(extrap2, [0, 0])
+
+    def test_euler_interpolation(self):
+        """Test euler complex interpolation"""
+        steps = np.arange(5)
+        interp_steps = steps[:-1] + 0.5
+        gain = np.array([1, 2, 3, 2, 1])
+        phase = np.radians([45, 90, 180, 90, 45])
+        interp = complex_interp(interp_steps, steps, gain*np.exp(1j*phase),
+                                method='euler')
+        gain_interp = np.interp(interp_steps, steps, gain)
+        phase_interp = np.interp(interp_steps, steps, phase)
+        assert np.allclose(np.abs(interp), gain_interp)
+        assert np.array_equal(np.angle(interp), phase_interp)
+
+    def test_euler_extrapolation(self):
+        """Test euler complex extrapolation"""
+        steps = np.arange(5)
+        extrap_steps = [steps[0]-1, steps[-1]+1]
+        gain = np.array([1, 2, 3, 2, 1])
+        phase = np.radians([45, 90, 180, 90, 45])
+        extrap = complex_interp(extrap_steps, steps, gain*np.exp(1j*phase),
+                                method='euler', outer=None)
+        assert np.allclose(np.abs(extrap), [gain[0], gain[-1]])
+        assert np.allclose(np.angle(extrap), [phase[0], phase[-1]])
+        extrap2 = complex_interp(extrap_steps, steps, gain*np.exp(1j*phase),
+                                 method='euler', outer=0)
+        assert np.array_equal(extrap2, [0, 0])
+
+    def test_euler_phase_unwrapping(self):
+        """Test phase unwrapping in euler complex interpolation"""
+        steps = np.arange(5)
+        interp_steps = steps[:-1] + 0.5
+        gain = np.array([1, 2, 3, 2, 1])
+        phase = np.radians([45, 90, 180, -90, -45])
+        interp = complex_interp(interp_steps, steps, gain*np.exp(1j*phase),
+                                method='euler')
+        phase_interp = np.interp(interp_steps, steps, np.unwrap(phase))
+        phase_interp = phase_interp%(2*np.pi)
+        phase_interp[phase_interp>np.pi] -= 2*np.pi
+        assert np.allclose(np.angle(interp), phase_interp)
+
+
+
+class Test_complex_bilinear_interp:
+    """Tests for complex_bilinear_interp function"""
+    def test_cartesian_interpolation(self):
+        """Test cartesian complex bilinear interpolation"""
+        x_steps = np.arange(5)
+        y_steps = np.arange(4)
+        x_interp_steps = x_steps[:-1] + 0.5
+        y_interp_steps = y_steps[:-1] + 0.5
+        real = np.array([[[1, 2, 3, 2],
+                          [2, 4, 6, 4],
+                          [3, 6, 9, 6],
+                          [2, 3, 4, 3],
+                          [1, 2, 3, 2]]])
+        imag = np.array([[[3, 2, 1, 2],
+                          [4, 3, 2, 3],
+                          [5, 4, 3, 4],
+                          [4, 3, 3, 3],
+                          [3, 2, 1, 2]]])
+        average_kernel = np.array([[0.25, 0.25], [0.25, 0.25]])
+        real_interp = scipy.signal.convolve2d(real[0], average_kernel,
+                                              mode='valid')
+        imag_interp = scipy.signal.convolve2d(imag[0], average_kernel,
+                                              mode='valid')
+        for i, x in enumerate(x_interp_steps):
+            for j, y in enumerate(y_interp_steps):
+                interp = complex_bilinear_interp(x, y, x_steps, y_steps,
+                                                 real+1j*imag,
+                                                 method='cartesian')
+                assert np.real(interp)==real_interp[i, j]
+                assert np.imag(interp)==imag_interp[i, j]
+
+    def test_euler_interpolation(self):
+        """Test euler complex bilinear interpolation"""
+        x_steps = np.arange(5)
+        y_steps = np.arange(4)
+        x_interp_steps = x_steps[:-1] + 0.5
+        y_interp_steps = y_steps[:-1] + 0.5
+        gain = np.array([[[1, 2, 3, 2],
+                          [2, 4, 6, 4],
+                          [3, 6, 9, 6],
+                          [2, 3, 4, 3],
+                          [1, 2, 3, 2]]])
+        phase = np.radians([[[90, 45, 0, 45],
+                             [135, 90, 45, 90],
+                             [180, 135, 90, 45],
+                             [135, 90, 45, 90],
+                             [90, 45, 0, 45]]])
+        average_kernel = np.array([[0.25, 0.25], [0.25, 0.25]])
+        gain_interp = scipy.signal.convolve2d(gain[0], average_kernel,
+                                              mode='valid')
+        phase_interp = scipy.signal.convolve2d(phase[0], average_kernel,
+                                               mode='valid')
+        for i, x in enumerate(x_interp_steps):
+            for j, y in enumerate(y_interp_steps):
+                interp = complex_bilinear_interp(x, y, x_steps, y_steps,
+                                                 gain*np.exp(1j*phase),
+                                                 method='euler')
+                assert np.abs(interp)==pytest.approx(gain_interp[i, j])
+                assert np.angle(interp)==pytest.approx(phase_interp[i, j])
+
+    def test_euler_phase_unwrapping(self):
+        """Test phase unwrapping in euler complex bilinear interpolation"""
+        x_steps = np.arange(5)
+        y_steps = np.arange(4)
+        x_interp_steps = x_steps[:-1] + 0.5
+        y_interp_steps = y_steps[:-1] + 0.5
+        gain = np.array([[[1, 2, 3, 2],
+                          [2, 4, 6, 4],
+                          [3, 6, 9, 6],
+                          [2, 3, 4, 3],
+                          [1, 2, 3, 2]],
+                         [[1, 2, 3, 2],
+                          [2, 4, 6, 4],
+                          [3, 6, 9, 6],
+                          [2, 3, 4, 3],
+                          [1, 2, 3, 2]]])
+        phase = np.radians([[[90, 45, 0, 45],
+                             [135, 90, 45, 90],
+                             [180, 135, 90, 45],
+                             [135, 90, 45, 90],
+                             [90, 45, 0, 45]],
+                            [[90, 45, 0, 45],
+                             [45, 0, -45, 0],
+                             [0, -45, -90, -135],
+                             [-135, 180, 135, 180],
+                             [90, 45, 0, 45]]])
+        unwrapped = np.unwrap(phase, axis=0)
+        average_kernel = np.array([[0.25, 0.25], [0.25, 0.25]])
+        gain_interp_0 = scipy.signal.convolve2d(gain[0], average_kernel,
+                                                mode='valid')
+        gain_interp_1 = scipy.signal.convolve2d(gain[1], average_kernel,
+                                                mode='valid')
+        phase_interp_0 = scipy.signal.convolve2d(unwrapped[0], average_kernel,
+                                                 mode='valid')
+        phase_interp_1 = scipy.signal.convolve2d(unwrapped[1], average_kernel,
+                                                 mode='valid')
+        for i, x in enumerate(x_interp_steps):
+            for j, y in enumerate(y_interp_steps):
+                interp = complex_bilinear_interp(x, y, x_steps, y_steps,
+                                                 gain*np.exp(1j*phase),
+                                                 method='euler',
+                                                 unwrap_axis=0)
+                assert np.allclose(np.abs(interp), [gain_interp_0[i, j],
+                                                    gain_interp_1[i, j]])
+                assert np.allclose(np.angle(interp), [phase_interp_0[i, j],
+                                                      phase_interp_1[i, j]])
 
 
 
