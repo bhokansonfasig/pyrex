195b42b9dbbad09885b83ade3a3a1a2ed77349fc
Benjamin Hokanson-Fasig <bhokansonfasig@gmail.com>
2021-04-15 15:37:59 -0500

    Bumped version number to 1.10.0

--- pyrex/__about__.py
+++ pyrex/__about__.py
@@ -7,7 +7,7 @@ __modulename__ = "pyrex"
 
 __fullname__ = "PyREx"
 
-__version__ = "1.9.0"
+__version__ = "1.10.0"
 
 __long_description__ = """
 A Python package for simulation of neutrinos and radio antennas in ice.
