fd96ce926bb20de3a567b2c22983a1f7484026a3
Benjamin Hokanson-Fasig <bhokansonfasig@gmail.com>
2021-04-26 17:26:47 -0500

    Change development status to stable

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     description = about["__description__"],
     long_description = about["__long_description__"],
     classifiers = [
-        "Development Status :: 4 - Beta",
+        "Development Status :: 5 - Production/Stable",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3 :: Only",
         "License :: OSI Approved :: MIT License",
